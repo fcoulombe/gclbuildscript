7320ac13a0208c3a023002bdeda5cd5c2f4e3c1c
fcoulombe <fcoulombe@silentfalls.org>
2011-10-09 20:11:46 -0700

    franky added *code to better spawn subprocess *rsync builder for the unit test *possibility to transfer folders in the unittests *something to display the list of aliases

--- site_scons/site_tools/Wrappers.py
+++ site_scons/site_tools/Wrappers.py
@@ -22,33 +22,62 @@
 import RunProgram
 import os.path
 import SCons.Builder
+import subprocess
+
 
 def builder_unit_test(target, source, env):
     app = str(source[0].abspath)
-    if os.spawnl(os.P_WAIT, app, app)==0:
-        open(str(target[0]),'w').write("PASSED\n")
-    else:
+    cwd = os.getcwd()
+    #os.chdir(os.path.dirname(app))
+    print "[TEST]: %s" % app
+    p = subprocess.Popen([app], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=os.path.dirname(app))
+    stdout, stderr  = p.communicate()
+    print stdout
+    print stderr
+    if  stderr and len(stderr) != 0:
+        print stderr
         return 1
+    if p.returncode != 0:
+        print stderr
+        return 1
+    open(str(target[0]),'w').write("PASSED\n")
+    return 0
+    
+    #if os.spawnl(os.P_WAIT, app, app)==0:
+    #    open(str(target[0]),'w').write("PASSED\n")
+    #    #os.chdir(cwd)
+    #else:
+        #os.chdir(cwd)
+    #return 1
 
+def builder_rsync(target, source, env):
+    for t in target:
+        rsync = "/usr/bin/rsync --times --force --recursive --update --delete --progress"
+        print "%s  %s %s" % (rsync, t.srcnode().abspath, os.path.dirname(t.abspath))
+        os.system("%s  %s %s" % (rsync, t.srcnode().abspath, os.path.dirname(t.abspath)))
+    return 0
 
 def BuildFiles(self, src_files, dependencies):
     self.AppendDependenciesCFlags(dependencies)
     objs = self.Object(src_files)
     return objs
     
-def UnitTest(self, name, src_files, dependencies):
+def UnitTest(self, name, src_files, dependencies, data):
     prog = self.KProgram(name, src_files, dependencies)
+    rsyncedData = self.RSync(data, None)
+    self.AlwaysBuild(rsyncedData)
+    self.Depends(prog, rsyncedData)
     self.Test(name+".passed", prog)
-    self.Alias(name+"_run", name+".passed")
-    self.Alias("@run_all", self.Alias(name+"_run"))
+    self.KAlias(name+"_run", name+".passed")
+    self.KAlias("@run_all", self.KAlias(name+"_run"))
     
 def KProgram(self, name, src_files, dependencies):
     objs = self.BuildFiles(src_files, dependencies)
     self.AppendDependenciesLFlags(dependencies)
     prog = self.Program(name, objs)
     for dep in dependencies:
-        self.Depends(prog, self.Alias("@"+dep))
-    self.Alias("@"+name, prog)
+        self.Depends(prog, self.KAlias("@"+dep))
+    self.KAlias("@"+name, prog)
     return prog
     
 def KStaticLibrary(self, name,  src_files, dependencies):
@@ -56,13 +85,29 @@ def KStaticLibrary(self, name,  src_files, dependencies):
     self.AppendDependenciesLFlags(dependencies) 
     lib = self.StaticLibrary(name, objs)
     for dep in dependencies:
-        self.Depends(lib, self.Alias("@"+dep))
-    self.Alias("@"+name, lib)
+        self.Depends(lib, self.KAlias("@"+dep))
+    self.KAlias("@"+name, lib)
     return lib
+
+
+aliasSet = []
+def displayAliases(self):
+    print "ALIASES"
+    for a in list(set(aliasSet)):
+        print a
+    
+def KAlias(self, alias, targets=None):
+    aliasSet.append(alias)
+    if targets:
+        return self.Alias(alias, targets)
+    else:
+        return self.Alias(alias)
     
 def generate(env, *args, **kw):
     env.AddMethod(KProgram)
     env.AddMethod(KStaticLibrary)
+    env.AddMethod(KAlias)
+    env.AddMethod(displayAliases)
     env.AddMethod(UnitTest)
     env.AddMethod(BuildFiles)
     
@@ -70,7 +115,9 @@ def generate(env, *args, **kw):
     bld = env.Builder(action = builder_unit_test)
     env.Append(BUILDERS = {'Test' :  bld})
 
-
+    # Create a builder for RSync
+    rsyncBld = env.Builder(action = builder_rsync)
+    env.Append(BUILDERS = {'RSync' :  rsyncBld})
 
 def exists():
     print "we exists"
