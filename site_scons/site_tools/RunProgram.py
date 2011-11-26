ffddaa7cde306c35587dbaf8b8c93f5d4d1f7b7b
fcoulombe <fcoulombe@silentfalls.org>
2011-11-26 04:47:50 -0800

    franky changes how we obtain the stdout and err when spawning a program

--- site_scons/site_tools/RunProgram.py
+++ site_scons/site_tools/RunProgram.py
@@ -23,8 +23,6 @@ import subprocess
 
 def RunProgram(cmdLine):
     p = subprocess.Popen(cmdLine, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    stdout = []
-    for line in p.stdout.readlines():
-        stdout.append(line)
+    stdout, stderr = p.communicate()
     retval = p.wait()
-    return stdout
+    return stdout, stderr
