e224f2603b975d5d45660e2413e6e63943952085
Francois Coulombe <fcoulombe@silentfalls.org>
2012-07-18 18:48:11 -0600

    franky creates a robocopy builder to replace rsync on windows

--- site_scons/site_tools/RSync.py
+++ site_scons/site_tools/RSync.py
@@ -28,19 +28,19 @@ from SCons.Action import Action
 
 import RunProgram
 
-
-def builder_rsync(target, source, env):
+def builder_robocopy(target, source, env):
+    rsyncExe = ""
+    rsyncExe = "robocopy"
     #print "target %s" % target[0]
     #print "source %s" % source[0]
     #if the user has specified a source. usually when rsyncing to install dir
     if source:
         if not os.path.exists(target[0].abspath):
             RunProgram.RunProgram("mkdir -p %s" % target[0].abspath)
-        rsync = "/usr/bin/rsync --times --force --recursive --update --delete --progress"
-        cmdLine = "%s  %s %s" % (rsync, source[0].abspath, os.path.dirname(target[0].abspath)) 
-        #print  cmdLine
-        stdout, stderr, returncode = RunProgram.RunProgram(cmdLine)
-            
+        rsync = rsyncExe
+        cmdLine = "%s  %s %s /mir" % (rsync, source[0].abspath, target[0].abspath) 
+        print  cmdLine
+        stdout, stderr, returncode = RunProgram.RunProgram(cmdLine) 
         if stderr and len(stderr):
             print stderr
             return -1
@@ -49,11 +49,51 @@ def builder_rsync(target, source, env):
             #create folder if it doesn't exists
             if not os.path.exists(t.abspath):
                 RunProgram.RunProgram("mkdir -p %s" % t.abspath)
-            rsync = "/usr/bin/rsync --times --force --recursive --update --delete --progress"
-            cmdLine = "%s  %s %s" % (rsync, t.srcnode().abspath, os.path.dirname(t.abspath)) 
-            #print  cmdLine
+            rsync = rsyncExe 
+            cmdLine = "%s  %s %s /mir" % (rsync, t.srcnode().abspath, t.abspath) 
+            print  cmdLine
             stdout, stderr, returncode = RunProgram.RunProgram(cmdLine)
-            
+            if stderr and len(stderr):
+                print stderr
+                return -1
+    return 0
+
+def builder_rsync(target, source, env):
+    rsyncExe = ""
+    if env["PLATFORM"] == "win32":
+        rsyncExe = "robocopy"
+    else:
+        rsyncExe = "rsync  --times --force --recursive --update --delete --progress "
+    #print "target %s" % target[0]
+    #print "source %s" % source[0]
+    #if the user has specified a source. usually when rsyncing to install dir
+    if source:
+        if not os.path.exists(target[0].abspath):
+            RunProgram.RunProgram("mkdir -p %s" % target[0].abspath)
+        rsync = rsyncExe
+        cmdLine = "%s  %s %s" % (rsync, source[0].abspath, os.path.dirname(target[0].abspath)) 
+        print  cmdLine
+        stdout, stderr, returncode = RunProgram.RunProgram(cmdLine)
+        if returncode != 0:
+            print "there was a problem with rsync"
+            print stdout
+            return -1    
+        if stderr and len(stderr):
+            print stderr
+            return -1
+    else:
+        for t in target:
+            #create folder if it doesn't exists
+            if not os.path.exists(t.abspath):
+                RunProgram.RunProgram("mkdir -p %s" % t.abspath)
+            rsync = rsyncExe 
+            cmdLine = "%s  %s %s" % (rsync, t.srcnode().abspath, os.path.dirname(t.abspath)) 
+            print  cmdLine
+            stdout, stderr, returncode = RunProgram.RunProgram(cmdLine)
+            if returncode != 0:
+                print "there was a problem with rsync"
+                print stdout
+                return -1
             if stderr and len(stderr):
                 print stderr
                 return -1
@@ -62,7 +102,11 @@ def builder_rsync(target, source, env):
     
 def generate(env, *args, **kw):
     # Create a builder for RSync
-    rsyncBld =Builder(action = Action(builder_rsync, cmdstr='[rsync] $TARGET'))
+    if env["PLATFORM"] == "win32":
+        rsyncBld =Builder(action = Action(builder_robocopy, cmdstr='[rsync] $TARGET'))
+    else:
+        rsyncBld =Builder(action = Action(builder_rsync, cmdstr='[rsync] $TARGET'))
+    
     env.Append(BUILDERS = {'RSync' :  rsyncBld})
 
 def exists():
