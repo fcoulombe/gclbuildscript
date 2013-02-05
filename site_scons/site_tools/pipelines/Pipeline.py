93931b2be70a9a6a1ba73a4a16e0153a2c8276f3
fcoulombe <fcoulombe@silentfalls.org>
2013-02-05 12:19:23 -0300

    franky updates the copy builder so that it uses the copy command since thats all it really needs

--- site_scons/site_tools/pipelines/Pipeline.py
+++ site_scons/site_tools/pipelines/Pipeline.py
@@ -1,6 +1,6 @@
 import RunProgram
 
-def builder_copy(target, source, env):
+def builder_copyLinux(target, source, env):
     for t in target:
         rsync = "/usr/bin/rsync --times --force --recursive --update --delete --progress"
         cmdLine = "%s  %s %s" % (rsync, source[0].abspath, t.abspath) 
@@ -12,6 +12,20 @@ def builder_copy(target, source, env):
             return -1
     return 0
 
+def builder_copyWindows(target, source, env):
+    for t in target:
+        rsync = "copy"
+        cmdLine = "%s  %s %s" % (rsync, source[0].abspath, t.abspath) 
+        print cmdLine
+        #print  cmdLine
+        stdout, stderr, returncode = RunProgram.RunProgram(cmdLine)
+        
+        if stderr and len(stderr):
+            print stderr
+            return -1
+    return 0
+
+builder_copy = builder_copyWindows
 class Pipeline(object):
     def __init__(self):
         self.name = "VOID"
