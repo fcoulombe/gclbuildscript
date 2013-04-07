a5456317b60a64f88b45c382ef175acc06a6c138
fcoulombe <fcoulombe@silentfalls.org>
2013-04-07 18:56:42 -0300

    franky modifies the script pipeline so that it uses the luacompiler to process the scripts. it takes care of using the 64bits compiler if the current machine is 64bits since compiled luascripts are not portable

--- site_scons/site_tools/pipelines/ScriptPipeline.py
+++ site_scons/site_tools/pipelines/ScriptPipeline.py
@@ -1,12 +1,34 @@
 import os
+import platform
 import Pipeline
 import SCons.Builder
 from SCons.Builder import Builder
 import SCons.Action
 from SCons.Action import Action
+import RunProgram
 
 pipelineName = "Script"
+if platform.architecture()[0] == "64bit":
+    LuaCompilerExePath = "#3rdparty/lua/bin/luac64.exe"
+else:
+    LuaCompilerExePath = "#3rdparty/lua/bin/luac.exe"
 
+global LuaCompilerExe
+
+def builder_script(target, source, env):
+    global LuaCompilerExe
+    cmdLine = "%s -o %s %s" % (LuaCompilerExe, target[0].abspath, source[0].abspath) 
+    #print  cmdLine
+    stdout, stderr, returncode = RunProgram.RunProgram(cmdLine)
+        
+    print stdout
+    if stderr and len(stderr):
+        print stderr
+        return -1
+    if returncode != 0:
+        return 1
+    
+    return 0
 class ScriptPipeline(Pipeline.Pipeline):
     def __init__(self):
         self.name = pipelineName + "Pipeline"
@@ -31,7 +53,10 @@ class ScriptPipeline(Pipeline.Pipeline):
 
        
     def generate(seld, env):
-        scriptBld =Builder(action = Action(Pipeline.builder_copy, cmdstr='[script] $TARGET'), suffix='.luascript')
+        global LuaCompilerExe
+        LuaCompilerExe = env.File(LuaCompilerExePath).abspath
+    
+        scriptBld =Builder(action = Action(builder_script, cmdstr='[script] $TARGET'), suffix='.luascript')
         env.Append(BUILDERS = {pipelineName + 'Builder' :  scriptBld})
 
 pipeline = ScriptPipeline()
